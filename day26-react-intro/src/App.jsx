c9ad3ea66d41ba6f6d4c400a6879222648a2e21c
LakshmiNarasimhan273 <bachelorofphotography@gmail.com>
2026-09-04 21:56:38 +0530

    React Introduction

---
+++ day26-react-intro/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from './components/Home'
+
+function App() {
+  return (
+    <div>
+      <h1>Day 1 of React</h1>
+      <Home />
+      <Home />
+    </div>
+  )
+}
+
+export default App
+
+// rfce - react functional component export
